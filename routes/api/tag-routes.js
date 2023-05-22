07c06ddb8e35c4539732e2e3b641f20a63445c4b
Mia Reid <miareid@Mias-iMac.local>
2023-05-22 17:14:18 -0400

    fixed models

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag,findAll({
+  Tag.findAll({
     attributes: [
       'id',
       'tag_name'
